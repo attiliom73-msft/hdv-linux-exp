d815ee0599ed3478e64a646184e215c64c4d07f8
attiliom73-msft <attiliom@microsoft.com>
2018-07-27 16:57:04 -0700

    random experiments

--- pcicalc/module.c
+++ pcicalc/module.c
@@ -7,12 +7,15 @@
 #include <linux/fs.h>
 #include <linux/uaccess.h>
 #include <linux/string.h>
+#include <linux/pci.h>
 
 //unsigned long copy_from_user(void*, void*, unsigned long);
 //unsigned long copy_to_user(void*, const void*, unsigned long);
 
 #define DEVICE_MAJOR_NUMBER 200
 #define DEVICE_NAME "pcicalculator"
+#define PCI_VENDOR_ID_MICROSOFT 0x1414
+#define PCI_DEVICE_ID_CALCULATOR 0xabcd
 
 int init_module(void);
 void cleanup_module(void);
@@ -20,6 +23,28 @@ int device_open(struct inode*, struct file *);
 int device_release(struct inode*, struct file *);
 ssize_t device_read(struct file*, char*, size_t, loff_t*);
 ssize_t device_write(struct file*, const char*, size_t, loff_t*);
+int pci_probe (struct pci_dev*, const struct pci_device_id*);
+void pci_remove(struct pci_dev*);
+
+struct pci_device_id pcicalc_ids[] = {
+    { PCI_DEVICE(0x1414, 0xabcd) },
+    { 0 }
+};
+
+MODULE_DEVICE_TABLE(pci, pcicalc_ids);
+
+struct pci_driver pci_driver = {
+    .name = "pcicalc",
+    .id_table = pcicalc_ids,
+    .probe = pci_probe,
+    .remove = pci_remove
+};
+
+struct dev_instance {
+    void* bar0;
+    void* buffer;
+    unsigned long buffer_physical_address;
+};
 
 struct file_operations g_FileOps = {
     .read = device_read,
@@ -30,6 +55,7 @@ struct file_operations g_FileOps = {
 
 char* g_operation_result = NULL;
 size_t g_operation_result_size = 0;
+struct dev_instance g_dev = { 0 };
 
 int init_module(void)
 {
@@ -49,13 +75,22 @@ int init_module(void)
     result = register_chrdev(DEVICE_MAJOR_NUMBER, DEVICE_NAME, &g_FileOps);
     if (result < 0) {
         pr_alert("register_chrdev failed: %d\n", result);
-        return result;
-    }	
+        goto cleanup;
+    }
+    
+    result = pci_register_driver(&pci_driver);
+    if (result < 0) {
+        pr_alert("pci_register_driver failed: %d\n", result);
+        goto cleanup;
+    }    
 
 cleanup:
 
-    if (result < 0 && g_operation_result != NULL) {
-        kfree(g_operation_result);
+    if (result < 0) {
+        if (g_operation_result != NULL) {
+            kfree(g_operation_result);
+        }
+        unregister_chrdev(DEVICE_MAJOR_NUMBER, DEVICE_NAME);
     }
 
     return result;
@@ -63,9 +98,86 @@ cleanup:
 
 void cleanup_module(void)
 {
+    pci_unregister_driver(&pci_driver);    
     unregister_chrdev(DEVICE_MAJOR_NUMBER, DEVICE_NAME);
     kfree(g_operation_result);
     pr_info("module unloaded\n");
+}
+
+int pci_probe (struct pci_dev* dev, const struct pci_device_id* id)
+{
+    int result;
+    unsigned long bar0_start;
+    unsigned long bar0_end;
+    u32 bar0;
+
+    pr_info("pci probe\n");
+    
+    if (id->vendor != PCI_VENDOR_ID_MICROSOFT ||
+        id->device != PCI_DEVICE_ID_CALCULATOR)
+    {
+	    return -1;
+	}
+    
+    pr_info("pci device found\n");
+    
+    result = pci_request_mem_regions(dev, "calc");
+    if (result != 0) {
+        pr_alert("pci_request_mem_regions failed: %d\n", result);
+        return result;
+    }
+    
+    bar0_start = pci_resource_start(dev, 0);
+    bar0_end = pci_resource_end(dev, 0);
+    pr_info("resource: BAR0 = %lx - %lx\n", bar0_start, bar0_end);
+    
+    pci_read_config_dword(dev, 0x10, &bar0);
+    pr_info("config space: BAR0 = %x\n", bar0);
+    /*
+    g_dev.bar0 = pci_iomap(dev, 0, 4096);
+    if (g_dev.bar0 == NULL) {
+        pr_alert("pci_iomap failed\n");
+        return -ENOMEM;
+    }
+    */
+    g_dev.bar0 = ioremap(bar0, 4096);
+    if (g_dev.bar0 == NULL) {
+        pr_alert("ioremap failed\n");
+        return -ENOMEM;
+    }
+    pr_info("BAR0 VA = %lx\n", (unsigned long)g_dev.bar0);
+    
+    g_dev.buffer = kmalloc(4096, GFP_DMA);
+    if (g_dev.buffer == NULL) {
+        pr_alert("kmalloc failed\n");
+        return -ENOMEM;
+    }
+    pr_info("buffer VA = %lx\n", (unsigned long)g_dev.buffer);
+    /*    
+    result = mlock(g_dev.buffer, 4096);
+    if (result < 0) {
+        pr_alert("mlock failed\n");
+        return -ENOMEM;
+    }*/
+    g_dev.buffer_physical_address = virt_to_phys(g_dev.buffer);
+    pr_info("buffer physical address = %lx\n", g_dev.buffer_physical_address);        
+
+    result = pci_enable_device(dev);
+    if (result != 0) {
+        pr_alert("pci_enable_device failed: %d\n", result);
+        return result;
+    }
+    pr_info("pci device enabled\n");
+
+    
+    writel((unsigned int)g_dev.buffer_physical_address, g_dev.bar0);
+    
+    
+    return result;        
+}
+
+void pci_remove(struct pci_dev* dev)
+{
 }
 
 int device_open(struct inode* inode, struct file* file)
